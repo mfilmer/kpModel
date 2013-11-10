cc2ae99af74ae6df8ad903aad0e3abe04ea24e6d
Matthew Filmer <mattfilmer@gmail.com>
2013-11-10 15:05:11 -0500

    Works now

--- kpModel.hs
+++ kpModel.hs
@@ -1,6 +1,9 @@
 import qualified Newton as N
 
 -- Constants
+m_0 = 9.10938           -- [kg] mass of electron
+hbar = 1.054572e-34     -- [Js] reduced Planck constant
+q = 1.602177e-19        -- [C] elementary charge
 
 -- Helper Functions
 log10 = logBase 10
@@ -9,10 +12,14 @@ logspace start stop count = [10**x | x <- [lstart, lstart + lstep .. lstop]]
     lstart = log10 start
     lstop = log10 stop
     lstep = (lstop - lstart) / (count - 1)
+jToEV joules = joules / q 
 
 charEqu p x = p * sin x / x + cos x + 1
-pArray = logspace 0.1 1000 100
+pArray = logspace 0.1 1000 1000
 equArray = map charEqu pArray
-egArray = map (\x -> N.solve x (1.5*pi)) equArray
+alphaAArray = map (\x -> N.solve x (N.guess x (1.1*pi, 2*pi))) equArray
+egArray = map e alphaAArray
+  where
+    e alpha = alpha - pi
 
-main = mapM putStrLn [show x ++ ", " ++ show y |(x,y) <- zip pArray egArray]
+main = mapM_ putStrLn [show x ++ ", " ++ show y |(x,y) <- zip pArray egArray]
--- newton.hs
+++ newton.hs
@@ -1,7 +1,8 @@
-module Newton where
---module Newton (step, solve, slope) where
+module Newton (step, solve, slope, guess) where
+import Data.List
+import Data.Maybe
 
--- Made up number for epsilon
+-- Made up number for epsilon, probably shouldn't call it epsilon... that's confusing
 epsilon = 1e-5
 slope :: (Double -> Double) -> Double -> Double
 slope func x = (func (x+inc) - func (x-inc))/(2*inc)
@@ -17,3 +18,10 @@ step func x = (m*x - func x) / m
 
 solve :: (Double -> Double) -> Double -> Double
 solve func x = iterate (step func) x !! 99
+
+guess :: (Double -> Double) -> (Double, Double) -> Double
+guess func (start, stop) = xValues !! (fromJust $ elemIndex (minimum funcVals) funcVals)
+  where
+    step = (stop - start) / 101
+    xValues = [start, start + step .. stop]
+    funcVals = map (abs.func) xValues
